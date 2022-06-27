b0f2b32a71bfe587fa7c3748268911d27cdf8e72
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-27 21:02:45 +0700

    Add more database functions

--- database/database.py
+++ database/database.py
@@ -29,3 +29,43 @@ class Database:
         else:
             cursor.execute(statement)
         return cursor
+
+    def retrieve_rows(self, statement: str, values=None):
+        """Run a SQL query to retrieve rows in table."""
+        cursor = self.execute_statement(statement, values)
+        results = cursor.fetchall()
+        cursor.close()
+        if results is None:
+            results = []
+        return [dict(r) for r in results]
+
+    def retrieve_single(self, statement: str, values=None):
+        """Run a SQL query to retrieve a single row in table."""
+        cursor = self.execute_statement(statement, values)
+        result = cursor.fetchone()
+        cursor.close()
+        if result is None:
+            result = []
+        return dict(result)
+
+    def write_rows(self, statement: str):
+        """Run a SQL query to update rows in table."""
+        cursor = self.execute_statement(statement)
+        self.connection.commit()
+        cursor.close()
+        return f"{cursor.rowcount} rows affected."
+
+    def insert_row(self, statement: str, values=None):
+        """Run a SQL query to add row to the table and return row id."""
+        # Format: "INSERT INTO table(attr) VALUES (attr) RETURNING id.
+        cursor = self.execute_statement(statement, values)
+        add_id = cursor.fetchone()[0]
+        self.connection.commit()
+        cursor.close()
+        return add_id
+
+    def delete_row(self, statement: str, values=None):
+        cursor = self.execute_statement(statement, values)
+        self.connection.commit()
+        cursor.close()
+        return
