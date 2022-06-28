d73685bbd9bdb1fe2e67352869719af9ea59259c
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-28 18:43:42 +0700

    Redo basic login function with sqlalchemy

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/twitter_db"
+database = SQLAlchemy()
 
 if __name__ == '__main__':
+    database.init_app(app)
+    database.create_all()
     # run app in debug mode on port 8082
     app.run(debug=True, port=8082)
