8b94ad1cc7749ab5149c82e02fafcda5c79de33b
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-28 16:02:54 +0700

    Add auth controller blueprint to main app and rename main database file

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -1,12 +1,14 @@
+from flask import Blueprint
 from flask import request
 
-from app import app
 from model.user_account import UserAccount
 from service import auth_svc
 
+auth_controller = Blueprint('auth_controller', __name__)
 
-@app.route('/register', methods=['POST'])
-def register_controller():
+
+@auth_controller.route('/register', methods=['POST'])
+def register_route():
     data = request.json
     keys = ('username', 'password', 'firstname', 'lastname', 'phone_number')
     if data and all([k in data for k in keys]):
@@ -16,8 +18,8 @@ def register_controller():
     return {'status': False}
 
 
-@app.route('/auth', methods=['POST'])
-def auth_controller():
+@auth_controller.route('/auth', methods=['POST'])
+def auth_route():
     credentials = request.json
     if credentials:
         status = auth_svc.authenticate(credentials.get('username'), credentials.get('password'))
--- database/user_db.py
+++ database/user_db.py
@@ -1,4 +1,4 @@
-from database import Database
+from db import Database
 
 database = Database()
 
