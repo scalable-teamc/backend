dc5f0d80360366497f386d94e39875bdab6f26fc
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-28 20:23:26 +0700

    Improve on how to connect to database and integrate flask login

--- service/auth_svc.py
+++ service/auth_svc.py
@@ -1,29 +1,29 @@
-import hashlib
+from flask_login import login_user
+from flask_sqlalchemy import SQLAlchemy
 
-from app import database
 from model.user_account import UserAccount
 
+database = SQLAlchemy()
 
-def authenticate(username: str, password: str) -> bool:
+
+def authenticate(username: str, password: str):
     if not username or not password:
         return False
-    db_user: UserAccount = database.session.query(UserAccount).filter_by(username=username).first()
-    if db_user and compare_password(password, db_user.password):
-        print("Successfully Login as {}".format(username))
-        return True
-    return False
-
-
-def compare_password(input_password: str, db_hash_password: str) -> bool:
-    input_hash_password = hashlib.md5(input_password.encode()).hexdigest()
-    return input_hash_password == db_hash_password
+    user: UserAccount = database.session.query(UserAccount).filter_by(username=username).first()
+    if user and user.verify_password(password):
+        login_user(user)
+        return {"message": "Successfully Login as {}".format(username)}
+    return {"message": "Login Fail"}
 
 
 def register(username: str, password: str):
-    hash_password = hashlib.md5(password.encode()).hexdigest()
-    new_user = UserAccount(username, hash_password)
+    new_user = UserAccount(username, password)
     database.session.add(new_user)
     database.session.commit()
-    if database.session.query(UserAccount.id).filter_by(username=username).first() is not None:
+    if user_exist(username):
         return {"message": f"User {new_user.username} has been created successfully."}
     return {"message": f" Fail to create User {new_user.username}."}
+
+
+def user_exist(username: str):
+    return database.session.query(UserAccount.id).filter_by(username=username).first() is not None
