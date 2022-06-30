9249b9edbf1bdacff9a55e4787a169e50f33a429
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-06-30 12:35:49 +0700

    Implement follow service

--- model/user_follow.py
+++ model/user_follow.py
@@ -16,9 +16,3 @@ class UserFollow(database.Model):
 
     def __repr__(self):
         return "follower IDs {}\nfollowing IDs {}".format(self.follower, self.following)
-
-    def add_follower(self, new_follower_id):
-        self.follower += [new_follower_id]
-
-    def add_following(self, new_following_id):
-        self.following += [new_following_id]
