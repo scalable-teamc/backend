8d945b63b02f66bb8a6d8039395b283ff5d5ab9e
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-06-30 12:38:49 +0700

    TODO: Function needed to be added -> add_picture(), add_video()

--- service/post.py
+++ service/post.py
@@ -4,6 +4,7 @@ import uuid
 import json
 from datetime import datetime
 import psycopg2
+from storage import MINIO_CLIENT
 
 from model import database as db
 from model.post_model import PasteModel
@@ -23,10 +24,10 @@ def create_post(json_data):
 
 # Get specific post (postID identifier for each Tweet). Return dict
 def get_specific_post(postID: str):
-        result = PasteModel.query.filter_by(postID=postID).first()
-        if not result:
-            return None
-        return result.to_dict()
+    result = PasteModel.query.filter_by(postID=postID).first()
+    if not result:
+        return None
+    return result.to_dict()
 
 
 # Show most recent tweets. Return array of dict
@@ -40,6 +41,9 @@ def get_recent_posts():
         arr.append(i.to_dict())
     return arr
 
+# TODO: Function needed to be added -> add_picture(), add_video()
+# Added function
+# attach picture
 
 
 
