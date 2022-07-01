1a5a0243580c019f19c0c4d6bfec26dfaee5de2e
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-07-01 19:59:34 +0700

    Fixing file and directory naming conflict causing import problems

--- auth_service/auth_svc.py
+++ auth_service/auth_svc.py
@@ -1,8 +1,8 @@
 from flask_login import login_user
-from storage import MINIO_CLIENT
 
-from user_account import UserAccount
+from storage import MINIO_CLIENT
 from . import user_db as database
+from .user_account import UserAccount
 
 
 def authenticate(username: str, password: str):
