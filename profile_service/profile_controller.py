f596b43b870efba4a54bd9ea9b74f21725c2c0e2
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-07-04 21:12:21 +0700

    Update to sync with frontend

--- profile_service/profile_controller.py
+++ profile_service/profile_controller.py
@@ -4,9 +4,12 @@ from .profile import *
 
 profile_controller = Blueprint('profile_controller', __name__)
 
+
 @profile_controller.route('/profile/save', methods=['POST'])
 def get_avatar():
 
     data = request.get_json()
-    save_avatar(data, 'test')
-    return get_avatar_file('test')
+    uid = data['uid']
+    image = data['image']
+    save_avatar(uid, image)
+    return get_avatar_file(uid)
