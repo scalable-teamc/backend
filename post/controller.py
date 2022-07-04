436d5cd249b17d8cad532044daacb116a945c128
Niraporn Kovitaya <bam_28@hotmail.com>
2022-07-04 22:06:29 +0700

    Add flask_cors

--- post/controller.py
+++ post/controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,7 +9,8 @@ from datetime import datetime
 import psycopg2
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] =  "postgresql://postgres:postgres@postgres:5432/postgres"
+CORS(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:password@localhost:5432/postgres"
 db = SQLAlchemy(app)
 
 
@@ -17,57 +19,58 @@ class PasteModel(db.Model):
     userID = db.Column(db.Integer(), autoincrement=True)
     mediaID = db.Column(db.Integer(), autoincrement=True)
     content = db.Column(db.String())
-    createdAt = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow()) # .now()
+    createdAt = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow())  # .now()
 
     # Return as dict to be send over HTTP response
     def to_dict(self):
         return {
-            "postID" : self.postID,
-            "userID" : self.userID,
-            "mediaID" : self.mediaID,
-            "content" : self.content,
-            "createdAt" : self.createdAt
+            "postID": self.postID,
+            "userID": self.userID,
+            "mediaID": self.mediaID,
+            "content": self.content,
+            "createdAt": self.createdAt
         }
+
 
 @app.before_first_request
 def create_tables():
     db.create_all()
 
-# Taking data from user, create post, and store it 
+
+# Taking data from user, create post, and store it
 @app.route('/post', methods=['POST'])
 def post_api():
     json_data = request.get_json()
+
     new_paste = PasteModel(userID=json_data["userID"], mediaID=json_data["mediaID"], content=json_data["content"])
 
     # Sent json_data to database
     db.session.add(new_paste)
     db.session.commit()
 
-    return "Post created\n", 200 # return JSON data ID
+    return "Post created\n", 200  # return JSON data ID
+
 
 # Get specific post (postID identifier for each Tweet)
 @app.route('/get/<int:postID>', methods=['GET'])
 def get_api(postID):
-        result = PasteModel.query.filter_by(postID=postID).first()
-        if not result:
-            return "postID not found\n" , 404
-        return result.to_dict(), 200
+    result = PasteModel.query.filter_by(postID=postID).first()
+    if not result:
+        return "postID not found\n", 404
+    return result.to_dict(), 200
+
 
 # Show most recent tweets
 @app.route('/recent', methods=['POST'])
 def recent_api():
     result = PasteModel.query.order_by("createdAt").limit(100)
     if not result:
-        return "No post yet\n" , 404
+        return "No post yet\n", 404
 
     arr = []
     for i in result:
         arr.append(i.to_dict())
-    return {"lst" : arr}, 200
-
-
-
-
+    return {"lst": arr}, 200
 
 
 if __name__ == "__main__":
