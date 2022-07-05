74b1a621cb1c59f9352f93937aa28959fd7d479a
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-07-06 03:23:31 +0700

    /profile/save now save avatar, display name, description

--- profile_service/profile_controller.py
+++ profile_service/profile_controller.py
@@ -5,11 +5,28 @@ profile_controller = Blueprint('profile_controller', __name__)
 
 
 @profile_controller.route('/profile/save', methods=['POST'])
-def get_avatar():
+def save_profile():
+
 
     data = request.get_json()
     uid = data['uid']
     image = data['image']
     ctype = data['type']
+    display_name = data['display_name']
+    description = data['description']
     save_avatar(uid, image, ctype)
+    add_display_name(uid,display_name)
+    add_description(uid, description)
     return 'f'
+
+
+@profile_controller.route('/profile/pic', methods=['GET'])
+def get_pic():
+
+    # frontend need to send uid somehow
+    data = request.get_json()
+    uid = data['uid']
+    picture = get_avatar(uid)
+
+    return picture
+
