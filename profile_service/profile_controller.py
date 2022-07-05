04cdeb0c87fd4661494a02e8114883f87eea718f
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-07-06 03:29:08 +0700

    add /profile to get description and display name

--- profile_service/profile_controller.py
+++ profile_service/profile_controller.py
@@ -7,7 +7,6 @@ profile_controller = Blueprint('profile_controller', __name__)
 @profile_controller.route('/profile/save', methods=['POST'])
 def save_profile():
 
-
     data = request.get_json()
     uid = data['uid']
     image = data['image']
@@ -30,3 +29,24 @@ def get_pic():
 
     return picture
 
+
+@profile_controller.route('/profile/disname', methods=['GET'])
+def get_disp():
+
+    # frontend need to send uid somehow
+    data = request.get_json()
+    uid = data['uid']
+    display_name = get_display_name(uid)
+
+    return display_name
+
+
+@profile_controller.route('/profile/description', methods=['GET'])
+def get_desc():
+
+    # frontend need to send uid somehow
+    data = request.get_json()
+    uid = data['uid']
+    description = get_description(uid)
+
+    return description
