edfacf662afc6779705ead85bab479d296df0104
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-07-06 23:35:55 +0700

    Add uid to link the user id account

--- profile_service/profile_account.py
+++ profile_service/profile_account.py
@@ -5,10 +5,12 @@ class UserProfile(database.Model):
     __tablename__ = 'profile'
 
     id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    uid = database.Column(database.Integer())
     display_name = database.Column(database.String())
     description = database.Column(database.Text())
 
-    def __init__(self, display_name, description):
+    def __init__(self, uid, display_name, description):
+        self.uid = uid
         self.display_name = display_name
         self.description = description
 
