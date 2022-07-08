64e7a559b902fa09f7bde885d04a2aef7de61d90
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-07-08 23:24:38 +0700

    Integrate follow functions into profile service

--- profile_service/profile_controller.py
+++ profile_service/profile_controller.py
@@ -1,6 +1,8 @@
 import json
+import logging
 
 from flask import Blueprint, request
+
 from .profile import *
 
 profile_controller = Blueprint('profile_controller', __name__)
@@ -8,7 +10,6 @@ profile_controller = Blueprint('profile_controller', __name__)
 
 @profile_controller.route('/profile/save', methods=['POST'])
 def save_profile():
-
     data = request.get_json()
     uid = data['uid']
     username = data['username']
@@ -24,7 +25,6 @@ def save_profile():
 
 @profile_controller.route('/profile/getprof', methods=['POST'])
 def get_profile():
-
     data = request.get_json()
     uid = data['uid']
     username = data['username']
@@ -60,3 +60,31 @@ def get_by_username():
     }
 
     return json.dumps(value)
+
+
+@profile_controller.route('/profile/following', methods=['POST'])
+def following():
+    data = request.json
+    uid: int = data["uid"]
+    following_id: int = data["following_id"]
+    add_following_response = add_new_following(uid, following_id)
+    logging.info(add_following_response["message"])
+    add_follower_response = add_new_follower(following_id, uid)
+    logging.info(add_follower_response["message"])
+    if add_follower_response["success"] and add_following_response["success"]:
+        return {"success": True, "message": "User:{} has follow User:{}".format(uid, following_id)}
+    remove_following(uid, following_id)
+    remove_follower(following_id, uid)
+    return {"success": False, "message": "User:{} fail to follow User:{}".format(uid, following_id)}
+
+
+@profile_controller.route("/profile/unfollow", methods=["PATCH"])
+def unfollow():
+    data = request.json
+    uid: int = data["uid"]
+    remove_id: int = data["remove_id"]
+    remove_following_response = remove_following(uid, remove_id)
+    logging.info(remove_following_response["message"])
+    remove_follower_response = remove_follower(remove_id, uid)
+    logging.info(remove_follower_response["message"])
+    return {"success": True}
