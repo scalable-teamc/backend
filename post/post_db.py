2c1d3bf9633257239827dcf23b9b59a74beb3b33
Niraporn Kovitaya <bam_28@hotmail.com>
2022-07-21 21:55:47 +0700

    Revert date format

--- post/post_db.py
+++ post/post_db.py
@@ -19,6 +19,6 @@ class PasteModel(db.Model):
             "username": self.username,
             "likedUser": self.likedUser,
             "content": self.content,
-            "createdAt": func.to_char(self.createdAt, '%Y-%m-%d %H:%M')
+            "createdAt": self.createdAt
         }
 
